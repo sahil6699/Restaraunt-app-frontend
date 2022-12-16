d1ee83e75dd3b4bbd1d531bf9f22474a64bb0ac5
sahilaneja0 <sahil@anubudh.com>
2022-12-16 12:05:46 +0530

    complete restaurant app

--- src/component/Basics/Resturant.js
+++ src/component/Basics/Resturant.js
@@ -1,13 +1,41 @@
 import React, { useState } from 'react';
 import './style.css';
-import Menu from './MenuApi';
+import Menu from './menuApi.js';
 import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  'All',
+];
+
+console.log(uniqueList);
 
 const Resturant = () => {
   const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === 'All') {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
   return (
     <>
-      <MenuCard menuData={menuData}></MenuCard>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
     </>
   );
 };
